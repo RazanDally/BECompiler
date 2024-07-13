e6e50b403505eb91abaa2c2ac4c10845c4240f1b
jouwana <jojo_2561@hotmail.com>
2024-07-13 23:28:27 +0300

    AKKA HTTP - run filePath based on 'get', sent URL has '\' changed to '$'

--- src/main/scala/hmloc/MainApp.scala
+++ src/main/scala/hmloc/MainApp.scala
@@ -10,8 +10,8 @@ import akka.http.scaladsl.server.Route.seal
 
 import scala.util.Failure
 import scala.util.Success
-
 import hmloc.DFRunner._
+import os.Path
 object MainApp {
 
 
@@ -21,11 +21,11 @@ object MainApp {
     implicit val executionContext = system.executionContext
 
     // Define the route that takes a number as a value in the URL
-    val route: Route =
+    val routes =
       pathPrefix("runTest") {
         path(IntNumber) { number =>
           get {
-            val output = runAllFiles(number)
+            val output = runTestFile(number)
             complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<code><pre> $output </pre></code>"))
           }
         } ~
@@ -35,9 +35,21 @@ object MainApp {
               complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Write runTest/ a number from 1 - 3 in link"))
             }
           }
+      } ~
+      pathPrefix("runPath") {
+        path(Segment) { mypath =>
+          get {
+            //change $ in mypath to / in path
+            val path = os.Path.apply(mypath.replace('$', '/'))
+
+            val output = runGivenPath(path)
+            // run the test
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<code><pre> $output </pre></code>"))
+          }
+        }
       }
 
-    val futureBinding = Http().newServerAt("localhost", 8080).bind(route)
+    val futureBinding = Http().newServerAt("localhost", 8080).bind(routes)
     futureBinding.onComplete {
       case Success(binding) =>
         val address = binding.localAddress
